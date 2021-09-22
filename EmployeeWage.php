099b54387dbd4db73e7001c10f7690e79002ce37
Niraj-kumar99 <kumarnkj35@gmail.com>
2021-09-22 11:22:03 +0530

    Added: Employee attendance check

--- EmployeeWage.php
+++ EmployeeWage.php
@@ -1,3 +1,10 @@
 <?php
-echo "Welcome to employee Wage Prob";
+echo "Welcome to employee Wage Prob\n";
+$attendance = rand(0,1);
+if ($attendance == 0) {
+    echo "Employee is ABSENT\n";
+}
+else {
+    echo "Employee is PRESENT\n";
+}
 ?>
