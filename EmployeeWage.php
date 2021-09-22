839e5514610469f64a484368258f51bd506bb774
Niraj-kumar99 <kumarnkj35@gmail.com>
2021-09-23 02:33:27 +0530

    Refactored: Calculating wages untill the given condition

--- EmployeeWage.php
+++ EmployeeWage.php
@@ -2,21 +2,32 @@
 $attendance = rand(0,1);
 $employeeType = rand(0,1);
 $wage_per_hr = 20;
+$max_days = 20;
+$EmployeeMonthlyWage =0;
+//$totalWorkingHr = 0;
 
-if ($attendance == 1){
+//Calculating daily wage per month
+for($i=0 ; $i<$max_days; $i++) {
     switch($employeeType) {
         case 0:
-            echo "\nPart Time Employee";
+            $type = "Part Time";
             $workingHr = 4;
             break;
         case 1:
-            echo "\nFull Time Employee";
+            $type = "Full Time";
             $workingHr = 8;
+            break;
+        default:
+            $workingHr = 0;
     }
+    //Calculating wage till this condition.
+    if($workingHr >= 100 || $i == 20) {
+        break;
+    }
+    $EmployeeWage = $wage_per_hr * $workingHr;
+    $EmployeeMonthlyWage += $EmployeeWage;
+    $max_days++;
 }
-else {
-    echo "\nEmployee is ABSENT";
-}
-$EmployeeWage = $wage_per_hr * $workingHr;
-echo ("\nTotal Wage : " .$EmployeeWage);
+echo "Employee Tyoe is ".$type ."\nTotal Working Days: ".$i ."\nTotal Working Hours:".$workingHr ."\nSalary Per Month: ".$EmployeeMonthlyWage;
+
 ?>
