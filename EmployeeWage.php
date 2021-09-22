adcd2905d601d2466cbc5a83a53fe3e0ee737864
Niraj-kumar99 <kumarnkj35@gmail.com>
2021-09-23 02:07:49 +0530

    Added: Calculating monthly wage

--- EmployeeWage.php
+++ EmployeeWage.php
@@ -2,21 +2,25 @@
 $attendance = rand(0,1);
 $employeeType = rand(0,1);
 $wage_per_hr = 20;
-
-if ($attendance == 1){
+$max_days = 20;
+$EmployeeMonthlyWage = 0;
+$MaxWorkingHr = 100;
+//Calculating daily wage per month
+for($i=0 ; $i<$max_days; $i++) {
     switch($employeeType) {
         case 0:
-            echo "\nPart Time Employee";
+            $type = "Part Time";
             $workingHr = 4;
             break;
         case 1:
-            echo "\nFull Time Employee";
+            $type = "Full Time";
             $workingHr = 8;
+            break;
+        default:
+            $workingHr = 0;
     }
+    $EmployeeWage = $wage_per_hr * $workingHr;
+    $EmployeeMonthlyWage += $EmployeeWage;
 }
-else {
-    echo "\nEmployee is ABSENT";
-}
-$EmployeeWage = $wage_per_hr * $workingHr;
-echo ("\nTotal Wage : " .$EmployeeWage);
+echo ("\nEmployee type is : ".$type ."\nMonthly Wage is : ".$EmployeeMonthlyWage);
 ?>
