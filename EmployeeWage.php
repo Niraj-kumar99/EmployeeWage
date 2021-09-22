ba837f7db94c8cc7f3e3de7b984faf048ed6ee69
Niraj-kumar99 <kumarnkj35@gmail.com>
2021-09-23 03:06:08 +0530

    Added: Class and attendance function added

--- EmployeeWage.php
+++ EmployeeWage.php
@@ -1,33 +1,18 @@
 <?php
 $attendance = rand(0,1);
 $employeeType = rand(0,1);
-$wage_per_hr = 20;
-$max_days = 20;
-$EmployeeMonthlyWage =0;
-//$totalWorkingHr = 0;
+class Employee {
+    function attendance($attendance) {
+        if ($attendance == 0) {
+            echo "Employee is ABSENT\n";
+        }
+        else {
+            echo "Employee is PRESENT\n";
+        }
+    }
 
-//Calculating daily wage per month
-for($i=0 ; $i<$max_days; $i++) {
-    switch($employeeType) {
-        case 0:
-            $type = "Part Time";
-            $workingHr = 4;
-            break;
-        case 1:
-            $type = "Full Time";
-            $workingHr = 8;
-            break;
-        default:
-            $workingHr = 0;
-    }
-    //Calculating wage till this condition.
-    if($workingHr >= 100 || $i == 20) {
-        break;
-    }
-    $EmployeeWage = $wage_per_hr * $workingHr;
-    $EmployeeMonthlyWage += $EmployeeWage;
-    $max_days++;
 }
-echo "Employee Tyoe is ".$type ."\nTotal Working Days: ".$i ."\nTotal Working Hours:".$workingHr ."\nSalary Per Month: ".$EmployeeMonthlyWage;
+$employee = new Employee();
+$employee->attendance($attendance);
 
 ?>
