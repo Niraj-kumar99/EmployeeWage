0149e56d93e8b52314b727c5b31b7685f2f0f7b9
Niraj-kumar99 <kumarnkj35@gmail.com>
2021-09-22 21:18:06 +0530

    Refactored: Part time Employee and wage calculation is added

--- EmployeeWage.php
+++ EmployeeWage.php
@@ -1,15 +1,20 @@
 <?php
-echo "Welcome to employee Wage Prob\n";
 $attendance = rand(0,1);
-$EmployeeWage =0;
-$wage_per_hr =20;
-if ($attendance == 0) {
-    echo "\n Employee is ABSENT2";
-    $workingHr = 0;
+$employeeType = rand(0,1);
+$wage_per_hr = 20;
+
+if ($attendance == 1) {
+    if ($employeeType == 0) {
+        echo "Part Time Employee";
+        $workingHr = 4;
+    }
+    else {
+        echo "Full Time Employee";
+        $workingHr = 8;
+    }
 }
 else {
-    echo "\nEmployee is PRESENT";
-    $workingHr = 8;
+    echo "\nEmployee is ABSENT";
 }
 $EmployeeWage = $wage_per_hr * $workingHr;
 echo ("\nTotal Wage : " .$EmployeeWage);
